1975a0acbd3f3fe4bff9b46e81ff1ace60d14ebc
truski22 <carlos.arias.carrasco@alumnos.upm.es>
2022-11-09 19:00:17 +0100

    signup endpoint 3

--- src/test/signup.java
+++ src/test/signup.java
@@ -18,6 +18,57 @@ import javax.ws.rs.core.Context;
 
 @Path("/signup")
 public class signup {
-	 @Context
-	    private UriInfo uriInfo;
+	@Context
+	private UriInfo uriInfo;
+
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response registrarUsuario(JsonObject json){
+		try{
+			DataBase db = new DataBase();
+
+
+			boolean active = rs.getBoolean("active");//DUDA
+			String email = rs.getString("email");
+			String username = rs.getString("username");
+			String password = rs.getString("password");
+			String gender = rs.getString("gender");
+			int age = rs.getInt("age");
+			String name = rs.getString("name");
+			String surname = rs.getString("surname");
+			String text_intro = rs.getString("text_intro");
+			Blob photo = rs.getBlob("photo");
+			String country = rs.getString("country");
+			String location = rs.getString("location");
+			
+			String sql=" INSERT INTO Usuario VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+            PreparedStatement ps = db.conn.prepareStatement(sql);
+            ps.setString(1,username);
+            ps.setString(2,password);
+			ps.setString(3,name);
+			ps.setString(4,surname);
+			ps.setInt(5,age);
+			ps.setString(6,text_intro);
+			ps.setString(7,email);
+			ps.setBlob(8,photo);
+			ps.setString(9,location);
+			ps.setString(10,country);
+			ps.setString(11,gender);
+			ps.setBoolean(12,active);
+            ResultSet rs = ps.executeQuery();
+
+			Login login = new Login();//cambiar el nombre a la clase al igual pero tiene los mismo parametros
+			login.setUsername(username);
+			login.setLogged(rs.next());//duda
+
+			Gson gson = new Gson();
+            String json = gson.toJson(login);
+
+			db.closeConn();
+            return Response.status(Response.Status.OK).entity(json).build();
+
+		}catch(Exception e){
+			return Response.status(Response.Status.NOT_FOUND).entity("no es posible registrar").header("Content-Location", uriInfo.getAbsolutePath()).build();
+		}
+	}
 }
