6626753c22af0dd5896de537fb70a98e05048748
truski22 <carlos.arias.carrasco@alumnos.upm.es>
2022-11-09 18:50:13 +0100

    login endponit 2

---
+++ src/interfaces/Login.java
@@ -0,0 +1,20 @@
+public class Login{
+    private String username;
+    private boolean logged;
+
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public boolean isLogged() {
+		return this.logged;
+	}
+
+	public void setLogged(boolean logged) {
+		this.logged = logged;
+	} 
+}
--- src/test/login.java
+++ src/test/login.java
@@ -15,7 +15,44 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Context;
+import interfaces.Login;
 @Path("/login")
 public class login {
+    @Context
+    private UriInfo uriInfo;    
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    //duda de si se pasa JsonObject o QueryParam
+    public Response loginUser(JsonObject json){
+        try{
+            DataBase db = new DataBase();
+
+            String username = json.getString("username");
+            String password = json.getString("password");
+
+            String sql=" Select username FROM Usuario where username = ? and password = ?;";
+            PreparedStatement ps = db.conn.prepareStatement(sql);
+            ps.setString(1,username);
+            ps.setString(2,password);
+            ResultSet rs = ps.executeQuery();
+
+            Login login = new Login();
+            login.setUsername(username);
+            login.setLogged(rs.next());
+
+            Gson gson = new Gson();
+            String json = gson.toJson(login);
+
+            db.closeConn();
+            return Response.status(Response.Status.OK).entity(json).build();
+
+        }catch(Exception e){
+            return Response.status(Response.Status.NOT_FOUND).entity("usuario-no-encontrado").header("Content-Location", uriInfo.getAbsolutePath()).build();
+        }
+
+        
+    }
+
 
 }
